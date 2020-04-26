648da49f6bebf0f3513237d52b004952ee453abe
Nolan Freeman <thenolanfreeman@icloud.com>
2020-04-26 06:38:59 -0500

    build structure for main.py and create scaffold file for entity, player and item

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 # main.py
+
+import pygame
+pygame.init()
+
+# GAME VARIABLES
+WINDOW_TITLE = "PySnake by Nolan Freeman"
+WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 1000
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.display.set_caption(WINDOW_TITLE)
+
+running = True
+
+# Main Game Loop
+while running:
+    # in milliseconds TODO replace with clock
+    pygame.time.delay(100)
+
+    for event in pygame.event_get():
+        # Break the game loop if QUIT event arises (eg. hitting 'x' on window)
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+# If loop is broken; quit game and close window
+pygame.quit()
