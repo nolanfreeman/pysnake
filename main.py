c4adcc3dd51ef8f31b256a9f64335a6c75983aef
Nolan Freeman <thenolanfreeman@protonmail.com>
2020-05-03 17:04:58 -0500

    pull scores for file and get powerups working

--- main.py
+++ main.py
@@ -4,35 +4,38 @@ import random
 import math
 
 class Block:
-    def __init__(self, pos, color, canvasdim):
-        self.size = BLOCK_SIZE
+    def __init__(self, pos, color, canvasdim, customwh=None):
+        if customwh:
+            self.size = customwh
+        else:
+            self.size = BLOCK_SIZE, BLOCK_SIZE
         self.speedx, self.speedy = 0,0
         self.color = color
         self.canvasdim = canvasdim
         self.posx, self.posy = self.get_random_pos(self.canvasdim) if pos == () else pos
 
     def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.posx, self.posy, self.size, self.size))
+        pygame.draw.rect(window, self.color, (self.posx, self.posy, self.size[0], self.size[1]))
 
     def update(self):
         self.posx += self.speedx
         self.posy += self.speedy
 
     def move(self, xydir):
-        self.speedx = xydir[0] * self.size
-        self.speedy = xydir[1] * self.size
+        self.speedx = xydir[0] * self.size[0]
+        self.speedy = xydir[1] * self.size[1]
 
     def transport(self, coordinates):
         self.posx, self.posy = self.get_random_pos(self.canvasdim) if coordinates == () else coordinates
 
     def get_random_pos(self, canvasdim):
-        x = random.randint(canvasdim[0], canvasdim[2]) // self.size * self.size
-        y = random.randint(canvasdim[1], canvasdim[3]) // self.size * self.size
+        x = random.randint(canvasdim[0], canvasdim[2]) // self.size[0] * self.size[0]
+        y = random.randint(canvasdim[1], canvasdim[3]) // self.size[1] * self.size[1]
         return x, y
 
     def distance(self, block):
-        bcenter = (block.posx+block.size/2, block.posy+block.size/2)
-        self.center = (self.posx+self.size/2, self.posy+self.size/2)
+        bcenter = (block.posx+block.size[0]/2, block.posy+block.size[1]/2)
+        self.center = (self.posx+self.size[0]/2, self.posy+self.size[1]/2)
         a = abs(self.center[0] - bcenter[0])
         b = abs(self.center[1] - bcenter[1])
         return math.sqrt(a**2 + b**2)
@@ -44,6 +47,7 @@ class Snake:
         self.pos = pos
         self.color = color
         self.canvasdim = canvasdim
+        self.inventory = [None, None, None, None]
 
     def draw(self, window):
         self.head.draw(window)
@@ -68,6 +72,34 @@ class Snake:
                 self.body[i-1].posx = self.body[i-2].posx
                 self.body[i-1].posy = self.body[i-2].posy
 
+    def pick_up(self, powerup):
+        for i, _ in enumerate(self.inventory):
+            if self.inventory[i] == None:
+                self.inventory[i] = powerup
+
+    def use_inventory(self, key, game):
+        if key == 'h':
+            index = 0
+        if key == 'j':
+            index = 1
+        if key == 'k':
+            index = 2
+        if key == 'l':
+            index = 3
+
+        if self.inventory[index] == None:
+            pass
+        else:
+            if self.inventory[index].item_type == 'w':
+                game.wraparound_on = True
+            elif self.inventory[index].item_type == 'd':
+                self.body = []
+            elif self.inventory[index].item_type == 'p':
+                game.passtrhough_on = True
+            elif self.inventory[index].item_type == 'm':
+                game.magnet_on = True
+            self.inventory[index] = None
+
     def add_block(self):
         new_block = Block((self.head.posx, self.head.posy), self.color, self.canvasdim)
         self.body.append(new_block)
@@ -77,15 +109,49 @@ class Snake:
 
     def body_collision(self):
         for block in self.body:
+            print(self.distance(block))
             if self.head.distance(block) < BLOCK_SIZE:
                  return True
-            else:
-                return False
         return False
-
 class Food(Block):
     def __init__(self, pos, color, canvasdim):
         super().__init__(pos, color, canvasdim)
+
+class Powerup(Block):
+    def __init__(self, item_type, color, canvasdim):
+        super().__init__((), color, canvasdim)
+        self.item_type = item_type
+        if item_type == 'w':
+            self.visthreshold = random.randint(1, 20)
+            pass
+        elif item_type == 'd':
+            self.visthreshold = random.randint(25, 60)
+            pass
+        elif item_type == 'p':
+            self.visthreshold = random.randint(8, 20)
+            pass
+        elif item_type == 'm':
+            self.visthreshold = random.randint(1, 4)
+            pass
+        print(self.visthreshold)
+
+        self.canvasdim = canvasdim
+        self.isvis = False
+
+    def isvisible(self, player_length):
+        if player_length > self.visthreshold:
+            self.isvis = True
+        else:
+            self.isvis = False
+        return self.isvis
+
+    def draw(self, window):
+        if self.isvis:
+            super().draw(window)
+
+    def update(self):
+        if self.isvis:
+            super().update()
 
 class TextBox:
     def __init__(self, text, font, size, color, pos, window):
@@ -134,20 +200,24 @@ class Game:
         self.theme = THEMES['default']
         self.state = 'play'
         self.score = 0
-        self.highscore = 0
+        self.highscore = int(open("scores.txt", 'r').read())
 
         self.num_foods = 1
         self.speed = 1
 
-        self.selected_items = [0,1,0,0,0,1,0]
+        self.selected_items = [0,1,1,1,0,2,0]
         self.players = []
         self.items = []
         self.obstacles = []
+        self.powerups = []
 
         self.menu_headers = []
         self.menu_items = []
 
         self.menu()
+
+        self.passthrough_on = False
+        self.magnet_on = False
 
     def main(self):
         while self.running:
@@ -176,11 +246,20 @@ class Game:
             self.score_text.draw()
             self.highscore_text.change_text("Highscore: " + str(self.highscore))
             self.highscore_text.draw()
-
+            for i, powerup_container in enumerate(self.powerups_container_array):
+                powerup_container.draw(self.window)
+            for i, powerup in enumerate(self.powerup_inventory_array):
+                if powerup:
+                    powerup.posx = self.powerups_container_array[i].posx + self.powerups_container_array[i].size[0]/4
+                    powerup.posy = self.powerups_container_array[i].posy + self.powerups_container_array[i].size[1]/4
+                    powerup.draw(self.window)
             for item in self.items:
                 item.draw(self.window)
             for obstacle in self.obstacles:
                 obstacle.draw(self.window)
+            for powerup in self.powerups:
+                if powerup:
+                    powerup.draw(self.window)
             for player in self.players:
                 player.draw(self.window)
         elif self.state == 'pause':
@@ -249,6 +328,23 @@ class Game:
             if keys[pygame.K_m]:
                 if self.state != 'menu':
                     self.menu()
+            if self.state == 'play' and self.powerups_on:
+                if keys[pygame.K_h]:
+                    for player in self.players:
+                        player.use_inventory('h', self)
+                        self.remove_powerup_from_screen(0)
+                if keys[pygame.K_j]:
+                    for player in self.players:
+                        player.use_inventory('j', self)
+                        self.remove_powerup_from_screen(1)
+                if keys[pygame.K_k]:
+                    for player in self.players:
+                        player.use_inventory('k', self)
+                        self.remove_powerup_from_screen(2)
+                if keys[pygame.K_l]:
+                    for player in self.players:
+                        player.use_inventory('l', self)
+                        self.remove_powerup_from_screen(3)
 
         if self.state == 'play':
             for player in self.players:
@@ -256,6 +352,13 @@ class Game:
                     obstacle.update()
                     if player.distance(obstacle) < BLOCK_SIZE:
                         self.gameover()
+                for i, powerup in enumerate(self.powerups):
+                    if powerup:
+                        if powerup.isvisible(len(player.body)+1):
+                            if player.distance(powerup) < BLOCK_SIZE:
+                                player.pick_up(powerup)
+                                self.add_powerup_to_screen(powerup)
+                                self.powerups[i] = None
                 player.update()
                 if player.head.posx < self.canvasdim[0] or player.head.posx > self.canvasdim[2] or player.head.posy < self.canvasdim[1] or player.head.posy >= self.canvasdim[3]:
                     if self.wraparound_on:
@@ -274,13 +377,33 @@ class Game:
                             player.head.posy %= self.canvasdim[3]
                     else:
                         self.gameover()
-                if player.body_collision():
-                    self.gameover()
+                if self.passthrough_on == False:
+                    if player.body_collision():
+                        self.gameover()
                 for item in self.items:
+                    if self.magnet_on:
+                        if player.head.posx > item.posx:
+                            dirx = 1
+                        elif player.head.posx == item.posx:
+                            dirx = 0
+                        elif player.head.posx < item.posx:
+                            dirx = -1
+                        if player.head.posy > item.posy:
+                            diry = 1
+                        elif player.head.posy == item.posy:
+                            diry = 0
+                        elif player.head.posy < item.posy:
+                            diry = -1
+                        item.move((dirx, diry))
+                        item.update()
                     if player.distance(item) < BLOCK_SIZE:
                         item.transport(())
                         if isinstance(item, Food):
                             player.add_block()
+                            if self.magnet_on:
+                                self.total_magnet_eats += 1
+                                if self.total_magnet_eats < 6:
+                                    self.magnet_on = False
                             self.score += int(10 * self.speed)
                             if self.score > self.highscore:
                                 self.highscore = self.score
@@ -313,6 +436,8 @@ class Game:
     def play(self):
         self.players = []
         self.items = []
+        self.score = 0
+        self.total_magnet_eats = 0
         self.state = 'play'
         player1 = Snake((), self.theme['player color'], self.canvasdim)
         self.players.append(player1)
@@ -321,10 +446,22 @@ class Game:
             self.items.append(food)
         self.highscore_text = TextBox("Highscore: " + str(self.highscore), self.theme['font'], 50, (0,0,0), ('margin-right-center', 5), self.window)
         self.score_text = TextBox("Score: " + str(self.score), self.theme['font'], 50, (0,0,0), ('margin-right-center', 50), self.window)
+
+        self.powerup_inventory_array = [None, None, None, None]
+        self.powerups_container_array = []
+        for i in range(4):
+            new_powerup_container = Block((20*i*2*1.1 +5,50), (100,100,100), self.canvasdim, customwh=(BLOCK_SIZE*2, BLOCK_SIZE*2))
+            self.powerups_container_array.append(new_powerup_container)
+
         if self.obstacles_on:
             for i in range(random.randint(1, 4)):
                 new_obstacle = Block((), self.theme['obstacle color'], self.canvasdim)
                 self.obstacles.append(new_obstacle)
+        magnet = Powerup('m', self.theme['magnet powerup color'], self.canvasdim)
+        passthrough = Powerup('p', self.theme['passthrough powerup color'], self.canvasdim)
+        del_body = Powerup('d', self.theme['del_body powerup color'], self.canvasdim)
+        wraparound = Powerup('w', self.theme['wraparound powerup color'], self.canvasdim)
+        self.powerups = [magnet, passthrough, del_body, wraparound]
 
     def pause(self):
         self.state = 'pause'
@@ -353,11 +490,22 @@ class Game:
 
         self.score = 0
         self.obstacles = []
+        scorefile = open("scores.txt", 'w')
+        scorefile.write(str(self.highscore))
+
+    def add_powerup_to_screen(self, powerup):
+        for i, _ in enumerate(self.powerup_inventory_array):
+            if self.powerup_inventory_array[i] == None:
+                self.powerup_inventory_array[i] = powerup
+                break
+
+    def remove_powerup_from_screen(self, index):
+        self.powerup_inventory_array[index] = None
 
 # Global Constants
 BLOCK_SIZE = 20
 HUD_HEIGHT = 100
-THEMES = {'default': {'font': 'Retro.ttf', 'player color': (0,0,0), 'food color': (255,0,0), 'menu title color': (0,0,0), 'obstacle color': (125,125,125), 'menu bg': (100,100,100), 'play bg': (0,0,255), 'pause bg': (100,100,255), 'gameover bg': (0,0,0), 'hud bg color': (0,0,150), 'menu item selected': (255,255,255)}}
+THEMES = {'default': {'font': 'Retro.ttf', 'player color': (0,0,0), 'food color': (255,0,0), 'menu title color': (0,0,0), 'obstacle color': (125,125,125), 'magnet powerup color': (100,100, 255), 'passthrough powerup color': (100,255,100), 'del_body powerup color': (255,100,100), 'wraparound powerup color': (255,255,100), 'menu bg': (100,100,100), 'play bg': (0,0,255), 'pause bg': (100,100,255), 'gameover bg': (0,0,0), 'hud bg color': (0,0,150), 'menu item selected': (255,255,255)}}
 STATES = ['menu', 'play', 'paused', 'gameover']
 
 MENU_TEXT = {'title': 'PySnake', 'Mode': ['Normal', 'Team', 'Battle', 'Zone'], 'Speed': ['0.5x', '1x', '1.5x', '2x'], 'Wraparound': ['off', 'on'], 'Power Ups': ['off', 'on'], 'Obstacles': ['off', 'on'], 'Num Food': ['1', '2', '3'], 'Map': ['1', '2', '3', '4']}
